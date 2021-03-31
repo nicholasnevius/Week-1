c76365503bae9da32e129d415ddfef56934ff6cd
Nick Nevius <nicknevius@yahoo.com>
2021-03-31 16:05:58 -0400

    Init commit

---
+++ fizzbuzz/FizzBuzzRunner.cs
@@ -0,0 +1,155 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Reflection.Emit;
+using System.Numerics;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace FizzBuzzRunner
+{
+    class FizzBuzzer
+    {
+        /// <summary>
+        /// Output:
+        ///     Apply rules to the num input and will return a string with that result
+        /// Inputs:
+        ///     num   = the current number in out FizzBuzz loop
+        ///     rules = A list of KeyValue<int, string> where the int is the multiple to run the rule on
+        ///             and string is the string to ensure shows up whenever printing a multiple of the int parameter
+        /// </summary>
+        private static String RunRules(int num, List<KeyValuePair<int, string>> rules)
+        {
+            string generated = "";
+            foreach (var rule in rules)
+            {
+                // The substring checking is important for preventing duplicates such as:
+                //      with rules (3, "Fizz") (5, "Buzz") (15, "FizzBuzz")
+                //      with num 15 would have printed "FizzBuzzFizzBuzz" without this check
+                // This is needed incase we have a number inbetween 3 and 15 in which case
+                // we cannot rely on a fallthrough from 3-5 to print "FizzBuzz" for the 15 case
+                if (num % rule.Key == 0 && !generated.Contains(rule.Value))
+                {
+                    generated += rule.Value;
+                }
+            }
+            return generated;
+        }
+
+        /// <summary>
+        /// Output:
+        ///     If num failed to match any rules then it will return a string of just num
+        ///     otherwise it will just return rules_appplied without modifications
+        /// Inputs:
+        ///     string rules_applied = the string after we ran HandleZero and ApplyRules
+        ///     num                  = the current nummber in our FizzBuzz loop
+        /// </summary>
+        private static string HandleRulesFailed(string rules_applied, int num)
+        {
+            if (rules_applied.Length == 0)
+            {
+                rules_applied += Convert.ToString(num);
+            }
+            return rules_applied;
+        }
+
+        /// <summary>
+        /// Reason:
+        ///     0 % ANYTHING is always 0 so it needs to be handled as a unique edgecase
+        /// Result:
+        ///     Will print out 0 if the num input is 0
+        /// Output:
+        ///     Will return true if the current num input is 0 and false otherwise
+        /// Inputs:
+        ///     num   = the current number in out FizzBuzz loop
+        /// </summary>
+        private static bool HandleZero(int num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine(Convert.ToString(num));
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Result:
+        ///     Will perform FizzBuzz on the current number while applying the rules to it
+        /// Inputs:
+        ///     num   = the current number in out FizzBuzz loop
+        ///     rules = A list of KeyValue<int, string> where the int is the multiple to run the rule on
+        ///             and string is the string to ensure shows up whenever printing a multiple of the int parameter
+        /// </summary>
+        private static void PrintNum(int num, List<KeyValuePair<int, string>> rules)
+        {
+            // zero must be handled differently
+            // will print to the console so no need to keep running this func
+            if (HandleZero(num))
+            {
+                return;
+            }
+
+            string rules_applied = RunRules(num, rules);
+            string formatted = HandleRulesFailed(rules_applied, num);
+            Console.WriteLine(formatted);
+        }
+
+        /// <summary>
+        /// Result:
+        ///     If it finds an error in the start + end or the rules parameters then it will throw an exception with an explanation
+        /// Inputs:
+        ///     start = number to start our FizzBuzz loop at
+        ///     end   = number to end our FizzBuzz loop at
+        ///     rules = A list of KeyValue<int, string> where the int is the multiple to run the rule on
+        ///             and string is the string to ensure shows up whenever printing a multiple of the int parameter
+        /// </summary>
+        private static void HandleInputErrors(int start, int end, List<KeyValuePair<int, string>> rules)
+        {
+            if (end < start)
+            {
+                throw new ArgumentException("End must be equal to or larger than start");
+            }
+            // TODO: decide if I should throw an exception or just print all numbers
+            if (rules == null)
+            {
+                throw new ArgumentException("Rules were null. If you wish to not apply rules please just pass an empty list");
+            }
+        }
+
+        /// <summary>
+        /// Result:
+        ///     Will perform FizzBuzz from start to end while applying rules to each number
+        /// Inputs:
+        ///     start = number to start at (can be negative)
+        ///     end   = numbet to end at (can be negative)
+        ///     rules = A list of KeyValue<int, string> where the int is the multiple to run the rule on
+        ///             and string is the string to ensure shows up whenever printing a multiple of the int parameter
+        /// </summary>
+        public static void RunFizzbuzz(int start, int end, List<KeyValuePair<int, string>> rules)
+        {
+            HandleInputErrors(start, end, rules);
+            var sorted_rules = rules.OrderBy(rule => rule.Key).ToList();
+            Enumerable.Range(start, end - start + 1).ToList().ForEach(i => PrintNum(i, sorted_rules));
+        }
+    }
+
+    // class with Main method to act as a runner for FizzBuzzer
+    class FizzBuzzRunner
+    {
+        static void Main(string[] args)
+        {
+            // rules are pairs of int (multiple) and string (thing to print)
+            List<KeyValuePair<int, string>> rules = new List<KeyValuePair<int, string>>();
+            rules.Add(new KeyValuePair<int, string>(3, "Fizz"));
+            rules.Add(new KeyValuePair<int, string>(5, "Buzz"));
+            rules.Add(new KeyValuePair<int, string>(15, "FizzBuzz"));
+
+            FizzBuzzer.RunFizzbuzz(-400, 600, rules);
+        }
+    }
+}
---
+++ palindrome/palindrome/Program.cs
@@ -0,0 +1,216 @@
+﻿using System.Security.Cryptography;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.ComTypes;
+using System;
+
+namespace palindrome
+{
+    enum PalindromeCalculationMethods
+    {
+        Reversing,
+        Recursion,
+        Efficient
+    }
+
+    class PalindromeCalculator
+    {
+        /// <summary>Will return the reverse of the inputted string
+        /// <param>
+        ///     input = the string you wish to reverse
+        /// </param>
+        /// <returns>
+        ///     a string that is the reverse of the input
+        /// </returns>
+        /// </summary>
+        private static string ReverseString(string input)
+        {
+            string reversed = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+            return reversed;
+        }
+
+        /// <summary> Check if a string is a palindrome by comparing it to its reverse
+        /// <param>
+        ///     input = the string you wish to check if it is a palindrome
+        /// </param>
+        /// <returns>
+        ///     True if input is a palindrome and false otherwise
+        /// </returns>
+        /// </summary>
+        private static bool IsPalindromeReverse(string input)
+        {
+            return input.Equals(ReverseString(input));
+        }
+
+        /// <summary>Check if a string is a palindrome by looping over half of it
+        /// <param>
+        ///     input = the string you wish to check if it is a palindrome
+        /// </param>
+        /// <returns>
+        ///     True if input is a palindrome and false otherwise
+        /// </returns>
+        /// </summary>
+        private static bool IsPalindromeEfficient(string input)
+        {
+            for (int i = 0, j = input.Length - 1; i <= j; i++, j--)
+            {
+                if (input[i] != input[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>The recursive function for checking if a string is a palindrome.
+        /// <param>
+        ///     input = the string you wish to check if it is a palindrome
+        ///             will shrink as we recurse
+        /// </param>
+        /// <returns>
+        ///     True if input is a palindrome and false otherwise
+        /// </returns>
+        /// </summary>
+        private static bool Recursing(string input)
+        {
+            if (input.Length < 2)
+            {
+                return true;
+            }
+            if (input[0] != input[input.Length - 1])
+            {
+                return false;
+            }
+            return Recursing(input.TrimEnd().TrimStart());
+        }
+
+        /// <summary>The entrypoint for checking if a string is a palindrome through recursion
+        /// <param>
+        ///     input = the string you wish to check if it is a palindrome
+        /// </param>
+        /// <returns>
+        ///     True if input is a palindrome and false otherwise
+        /// </returns>
+        /// </summary>
+        private static bool IsPalindromeRecursive(string input)
+        {
+            return Recursing(input);
+        }
+
+        /// <summary>Handle any errors in user input
+        /// <param>
+        ///     input = the string you wish to check for errors
+        /// </param>
+        /// <exception>
+        ///     Can throw an Exception if the input gotten from the user is somehow null
+        /// </exception>
+        /// </summary>
+        private static void HandleUserInputError(string input)
+        {
+            if (input == null)
+            {
+                throw new Exception("How did you even manage to type in null?");
+            }
+        }
+
+        /// <summary>Perform checks if string is trivially a palindrome
+        /// <param>
+        ///     input = the string you wish to check if it is a palindrome
+        /// </param>
+        /// <returns>
+        ///     True if input is a palindrome and false otherwise
+        /// </returns>
+        /// </summary>
+        private static bool InitialChecks(string input)
+        {
+            return input.Length < 2;
+        }
+
+        /// <summary>The entrypoint for checking if a string is a palindrome through recursion
+        /// <param>
+        ///     method = the method it will use to calculate for checking
+        /// </param>
+        /// <param>
+        ///     input = the string you wish to check if it is a palindrome
+        /// </param>
+        /// <returns>
+        ///     True if input is a palindrome and false otherwise
+        /// </returns>
+        /// </summary>
+        private static bool CheckCalculationMethod(PalindromeCalculationMethods method, string input)
+        {
+            switch (method)
+            {
+                case PalindromeCalculationMethods.Efficient:
+                    return IsPalindromeEfficient(input);
+                case PalindromeCalculationMethods.Recursion:
+                    return IsPalindromeRecursive(input);
+                case PalindromeCalculationMethods.Reversing:
+                    return IsPalindromeReverse(input);
+                default:
+                    return false; ;
+            }
+        }
+
+        /// <summary>Checks for a palindrome through the given method
+        /// <param>
+        ///     input = a string to check whether it is a palindrome or not
+        /// </param>
+        /// <param>
+        ///     method = The method (Recursion, Reversing, or Efficient) to calculate
+        /// </param>
+        /// <returns>
+        ///     True if input is a palindrome and false otherwise
+        /// </returns>
+        /// </summary>
+        public static bool IsPalindrome(string input, PalindromeCalculationMethods method)
+        {
+            HandleUserInputError(input);
+            if (InitialChecks(input) || CheckCalculationMethod(method, input))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>Prints PrintTrue if input is a palindrome or PrintFalse if it is not
+        /// <param>
+        ///     input = the string to check whether it is a palindrome or not
+        /// </param>
+        /// <param>
+        ///     PrintTrue = the string to print if input is a palindrome
+        /// </param>
+        /// <param>
+        ///     PrintFalse = the string to print if input is not a palindrome
+        /// </param>
+        /// <param>
+        ///     method = The calculation method (Recursive, Reversing, or Efficient) to use for calculation
+        /// </param>
+        /// </summary>
+        public static void PrintIsPalindrome(string input, string PrintTrue, string PrintFalse, PalindromeCalculationMethods method)
+        {
+            if (IsPalindrome(input, method))
+            {
+                Console.WriteLine(PrintTrue);
+            }
+            else
+            {
+                Console.WriteLine(PrintFalse);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user_input = Console.ReadLine();
+            PalindromeCalculator.PrintIsPalindrome(user_input, "true", "false", PalindromeCalculationMethods.Efficient);
+        }
+    }
+}
+
